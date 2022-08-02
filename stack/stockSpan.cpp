f797639cebae9a5d414c5066c0fcdd59cf1b3962
Abhay <abhaybisht9412@gmail.com>
2022-08-02 17:58:16 +0530

    intial commit

---
+++ stack/stockSpan.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+int main () {
+vector<int> a = {100,80,60,70,60,75,85};
+int n=7;
+vector<int> ans;
+for(int i=n-1;i>=0;i--){
+    int c=1;
+    for(int j=i-1 ; j>=0 ;j--){
+        if(a[j]<a[i]){
+            c++;
+        }
+        else{
+            ans.push_back(c) ;
+            break;
+      } 
+    }
+}
+ans.push_back(1);
+reverse(ans.begin(),ans.end());
+for(auto i:ans){
+    cout<<i<<" ";
+}
+}
