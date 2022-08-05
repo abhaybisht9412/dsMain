9afe8651e91f0dcdad1e27ea00285e0ee6522f7f
Abhay <abhaybisht9412@gmail.com>
2022-08-05 16:10:07 +0530

    commit

--- rainWaterTrapping.cpp
+++ rainWaterTrapping.cpp
@@ -2,21 +2,26 @@
 using namespace std ;
 
 int main() {
-vector<int> a = {3, 0, 0, 2, 0, 4};
-int n=6;
-vector<int> mxr,mxl;
-int maxi=INT_MIN;
+vector<int> a = {8, 8, 2 ,4 ,5, 5, 1};
+int n=7;
+vector<int> mxr(n),mxl(n);
+
 //mxl
-for(int i=0;i<n;i++){
-    maxi = max(maxi , a[i]);
-    mxl.push_back(maxi);
+mxl[0]=a[0];
+for(int i=1;i<n;i++){
+    mxl[i]=max(mxl[i-1],a[i]);
 }
 //mxr
-maxi=INT_MIN;
-for(int i=n-1;i>=0;i--){
-    maxi = max(maxi , a[i]);
-    mxr.push_back(maxi);
+mxr[n-1]=a[n-1];
+for(int i=n-2;i>=0;i--){
+    mxr[i]=max(mxr[i+1],a[i]);
 }
+// for(auto i:mxl){
+//     cout<<i<<" ";
+// }
+// cout<<endl;
+// for(auto i:mxr)
+// cout<<i<<" ";
 vector<int> mini ;
 for(int i=0;i<n;i++){
     mini.push_back(min(mxl[i],mxr[i]));
