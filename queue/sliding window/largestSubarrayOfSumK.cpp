b7d686653cbf1ed034f42640479f204b42d928fb
Abhay <abhaybisht9412@gmail.com>
2022-08-11 22:20:16 +0530

    commit

--- queue/sliding window/largestSubarrayOfSumK.cpp
+++ queue/sliding window/largestSubarrayOfSumK.cpp
@@ -1,28 +1,34 @@
-    #include<bits/stdc++.h>
+   #include<bits/stdc++.h>
     using namespace std;
 
       int main(){
       vector<int> a = {4,1,1,1,2,3,5} ;
        int n = 7 ;
        int target = 5;
-       int i=0,j=0;
-       vector<int> ans;
-       for(int i=0;i<n;i++){
-        int sum = 0;
-        for (int j = i; j < n; j++)
+        int sum = 0,max_sum=0;
+        int i=0,j=0;
+        while (j<n)
         {
             /* code */
             sum+=a[j];
-            if(sum > target){
-                break;
+            
+            if( sum < target){
+                j++;
+                continue;
             }
-            else if(sum == target){
-                ans.push_back(j-i+1);
-                break;
+
+            else if( sum == target){
+                max_sum = max(max_sum ,j-i+1);
             }
-            else continue;
+            else{
+                while(sum > target){
+                    sum-=a[i];
+                    i++;
+                }
+                if(sum == target)
+                max_sum = max(max_sum ,j-i+1);
+            }
+            j++;
         }
-       }
-       cout<<*max_element(ans.begin(),ans.end());
-         
-        }
+        cout<<max_sum;
+      }
